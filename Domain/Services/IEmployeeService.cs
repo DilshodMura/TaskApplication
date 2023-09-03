5445f9f6256b59724b2045d6bec293d47fb65905
unknown <muratovdilshod2@gmail.com>
2023-09-03 14:34:24 -0400

    test

---
+++ Domain/Services/IEmployeeService.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+using Domain.Repositories;
+
+namespace Domain.Services
+{
+    public interface IEmployeeService
+    {
+        public Task<IEnumerable<IEmployee>> ParseCsvFile(Stream stream);
+        public Task ImportEmployeesFromCsvAsync(Stream stream);
+        public Task<IEnumerable<IEmployee>> GetImportedEmployeesAsync();
+
+        public IEmployee GetEmployeeById(int employeeId);
+        public Task UpdateEmployee(IEmployee employee);
+    }
+}
